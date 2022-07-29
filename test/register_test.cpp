32093aa2948f35dd3e0a459c15f1d71f4e34055a
HansBlackCat <jeremy3599@snu.ac.kr>
2022-07-30 03:30:20 +0900

    Refactor deleter 1. use delete instead of free()

--- test/register_test.cpp
+++ test/register_test.cpp
@@ -13,9 +13,9 @@ TEST(RegisterTest, set_context) {
   auto reg = new Register{.rsp = 0, .rdx = (uint64_t)EntryPoint};
   uint64_t ls = set_context(reg);
   EXPECT_EQ(ls, 1);
-  std::cerr << STDERR_GTEST_INIT << "Hi" << '\n';
   uint64_t lw = switch_context(reg);
   EXPECT_EQ(lw, 1);
+  
 }
 
 }  // namespace
